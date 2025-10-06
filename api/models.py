d23a0f9e111063c7c35d83db76b0c0d07e401fe2
MostafaGhanbari9176 <godhelot1@outlook.com>
2025-10-06 19:32:29 +0330

    feat(api): implement and migrate models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class SchemaNames(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class ETLNames(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class TableNames(models.Model):
+    name = models.CharField(max_length=255)
+    schema = models.ForeignKey(SchemaNames, on_delete=models.CASCADE)
+    etls = models.ManyToManyField(ETLNames, through="ETLTableRel")
+
+    class Meta:
+        unique_together = (
+            "name",
+            "schema",
+        )  # table name must be unique per each schema
+
+
+class ETLTableRel(models.Model):
+    etl = models.ForeignKey(ETLNames, on_delete=models.CASCADE)
+    table = models.ForeignKey(TableNames, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("etl", "table")  # unique relations
