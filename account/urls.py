0c3c68d73f20a5d823886790f0a4c0c25c6aaa40
MostafaGhanbari9176 <godhelot1@outlook.com>
2025-10-07 12:02:22 +0330

    feat(auth): add refresh token endpoint

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,9 @@
+from rest_framework_simplejwt.views import TokenRefreshView 
 from django.urls import path
 from . import views as views
 
 urlpatterns = [
     path("log-in/", views.log_in, name="sign-in or sign-up"),
     path("log-in/confirm/", views.log_in_confirm, name="otp-validation"),
+    path("refresh/", TokenRefreshView.as_view(), name="token-refresh"),
 ]
